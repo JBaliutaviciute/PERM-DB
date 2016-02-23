e75d7951741d64a90d95f3d66909f31ec637689d
Povilas <p.treigys@eif.viko.lt>
2016-02-23 21:40:45 +0200

    Current Web solution working version.

--- Perm DB.sql
+++ Perm DB.sql
@@ -1,25 +1,33 @@
-/*==============================================================*/
-/* DBMS name:      Microsoft SQL Server 2008                    */
-/* Created on:     2016-02-15 14:59:26                          */
-/*==============================================================*/
+SE [PERM_DB]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 2016-02-23 21:28:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[userName] [nvarchar](255) NOT NULL,
+	[userPassword] [nvarchar](64) NOT NULL,
+	[salt] [nvarchar](24) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[surname] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK__User__3214EC077F60ED59] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
+GO
+/****** Object:  Table [dbo].[UserSessions]    Script Date: 2016-02-23 21:28:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserSessions](
+	[userID] [int] NOT NULL,
+	[userSession] [nvarchar](24) NOT NULL,
+	[sesionExpires] [datetime] NOT NULL
+) ON [PRIMARY]
 
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('"USER"')
-            and   type = 'U')
-   drop table "USER"
-go
-
-/*==============================================================*/
-/* Table: "USER"                                                */
-/*==============================================================*/
-create table "USER" (
-   USERNAME             nvarchar(255)        not null,
-   USERPASSWORD         nvarchar(64)         not null,
-   ID                   numeric(4)           identity,
-   SALT                 nvarchar(24)         not null,
-   constraint PK_USER primary key nonclustered (ID)
-)
-go
-
+GO
